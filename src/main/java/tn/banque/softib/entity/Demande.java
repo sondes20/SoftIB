053e13e06d6d7db97de4b18b7ecccbd8338e2ef9
sondes20 <lahouani.sondes@esprit.tn>
2021-07-18 22:28:05 +0200

    Ajout entité demande

---
+++ src/main/java/tn/banque/softib/entity/Demande.java
@@ -0,0 +1,12 @@
+package tn.banque.softib.entity;
+
+import java.io.Serializable;
+
+public class Demande implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
